c5482a067c7482c97bd43134baa2c16b14dfe372
Rizwan Riaz <iamrizwan077@gmail.com>
2022-05-08 10:01:21 +0500

    pizza app

--- imdex.js
+++ imdex.js
@@ -1,6 +1,47 @@
-const menu = document.querySelector('.menu')
+/*const menu = document.querySelector('.menu')
 const bar = document.querySelector('.bar')
 
 bar.addEventListener('click', function(){
     menu.styles
+})*/
+
+
+const pizza = document.getElementById('pizza')
+const salad = document.getElementById('salad')
+const starter = document.getElementById('starter')
+
+pizza.classList.add('active')
+pizza.addEventListener('click',function(){
+   
+    pizza.classList.add('active')
+    salad.classList.remove('active')
+    starter.classList.remove('active')
+    
+    document.getElementById('pizzamenu').classList.remove('menuhider')
+    document.getElementById('saladmenu').classList.add('menuhider')
+    document.getElementById('startermenu').classList.add('menuhider')
+    
+})
+
+salad.addEventListener('click',function(){
+
+    pizza.classList.remove('active')
+    salad.classList.add('active')
+    starter.classList.remove('active')
+    
+    document.getElementById('pizzamenu').classList.add('menuhider')
+    document.getElementById('saladmenu').classList.remove('menuhider')
+    document.getElementById('startermenu').classList.add('menuhider')
+    
+})
+starter.addEventListener('click',function(){
+
+    pizza.classList.remove('active')
+    salad.classList.remove('active')
+    starter.classList.add('active')
+  
+    document.getElementById('pizzamenu').classList.add('menuhider')
+    document.getElementById('saladmenu').classList.add('menuhider')
+    document.getElementById('startermenu').classList.remove('menuhider')
+    
 })
